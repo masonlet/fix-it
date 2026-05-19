31ef5a159665fe3d222ae9dd2b252c84485d5e7c
Masonlet <masonletoile@hotmail.com>
2026-05-19 19:22:46 -0400

    refactor: convert Boot scene to TypeScript

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { YouTubePlayables } from './scripts/sdk/youTubePlayables.ts';
 import { WaveDash } from './scripts/sdk/waveDash.ts';
 
-import { Boot } from './scripts/scenes/Boot';
+import { Boot } from './scripts/scenes/boot.ts';
 import { Preloader } from './scripts/scenes/Preloader';
 import { MainMenu } from './scripts/scenes/MainMenu';
 import { Game } from './scripts/scenes/Game';
